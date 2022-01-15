578b90751a49850acf5a3467f0b085d8fe043d6a
ayy-em <mironov-hse@yandex.ru>
2022-01-15 04:42:33 +0100

    basic testing handlers and cron jobs added

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # SomethingBot for Telegram
 
-Talk to me at https://t.me/SomethingReallyBot
+Talk to me at https://t.me/SomethingReallyBot at any time, day and night, baby, day and night.
 
-### What does it do?
+### What does this bot do?
 
-Things, that's what.
+Things, that's what. Just shoot me a message.
+
+### Channels you should totally check out
+
+- https://t.me/maymays_unlimited - English - Three memes a day, every day
+- https://t.me/vice_news - English - This bot scrapes Vice news and posts a story every day
+- https://t.me/rfn_didyouknow - English - "Did you know?" fresh fun facts and interesting trivia
+- https://t.me/adam24live - Russian - News, short stories and funny pics about Amsterdam
+- https://t.me/ayy_maps - Russian - Are you a fan of maps or a data geek?
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python
-import webapp2
+import requests
+
+from flask import Flask, request
+
+app = Flask(__name__)
 
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+@app.route('/', methods=['GET'])
+def get_root():
+    r = requests.get(
+        "https://api.telegram.org/bot1651613683:AAEsfNziIczgMWs122wYUyES_A-boBwK5SQ/sendMessage?chat_id=-1001626784462&text={}".format(
+            str(request.get_data())))
+    return "Hello there"
 
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+@app.route('/', methods=['POST'])
+def post_root():
+    r = requests.get("https://api.telegram.org/bot1651613683:AAEsfNziIczgMWs122wYUyES_A-boBwK5SQ/sendMessage?chat_id=-1001626784462&text={}".format(str(request.get_data())))
+    print("That post json here is: " + str(request.get_json()))
+    return "Hello you and your post call"
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
