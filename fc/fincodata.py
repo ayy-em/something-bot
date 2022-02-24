7975e3035652e77ae93b4ba08fa685591cdc70fb
ayy-em <mironov-hse@yandex.ru>
2022-02-24 23:13:04 +0100

    #1 - removed timer and added randomized greeting

--- fc/fincodata.py
+++ fc/fincodata.py
@@ -1,5 +1,5 @@
 import os
-import time
+import random
 
 import requests
 
@@ -27,21 +27,36 @@ def get_fc_message(query_name):
 
 def get_yesterday_text(query_period):
     data_json = get_fc_data(query_period)
-    text_one = '👋 ' + data_json['period']
+    greetings_list = [
+        "Good morning! 🌞 Here are the stats. ",
+        "Heya 👋 FC stats here. ",
+        "Morning bitches 😎 Eat stats! ",
+        "Are you ready to get these sweet 10 Euros a month?! 🤑 ",
+        "Let's get down to business 💼 \nI don't got no time to play around, what is this?\n",
+        "Did you post something on social today? 👀 ",
+        "ayy lmao 👽 fresh stats arrived ",
+        "Ciao miei cari amici! 🤌 🤌 🤌 ",
+        "Hello there boyz ✌️ "
+    ]
+    text_one = random.choice(greetings_list)
+    text_one = text_one + data_json['period']
     data_json.pop('period')
     clicks_total = len(data_json)
-    if clicks_total > 0:
-        text_two = "\n\nRedirected to partners: {} users. Here's a list:\n".format(str(clicks_total))
+    if 0 < clicks_total < 8:
+        text_two = "\n\nRedirects to partners: {}. Here's a list:\n".format(str(clicks_total))
+        for click in data_json:
+            text_line = '- ' + data_json[click]['when'][:5] + ' - ' + data_json[click]['where'] + ' - ' + \
+                        data_json[click]['uc']
+            text_two = text_two + text_line + '\n'
+    elif clicks_total >= 8:
+        text_two = "\n\nRedirects to partners: {}. Too many to list here.\n".format(str(clicks_total))
     else:
-        text_two = 'Redirected nobody to partners. Feels bad, man.'
-    for click in data_json:
-        text_line = '- ' + data_json[click]['when'][:5] + ' - ' + data_json[click]['where'] + ' - ' + data_json[click]['uc']
-        text_two = text_two + text_line + '\n'
+        text_two = '\nZero redirects to partners. Feels bad, man. 😔\n'
+
     text = text_one + text_two
     try:
-        text = text + get_and_format_ga_data()
+        text = text + '\n🤖 ' + get_and_format_ga_data()
     except Exception as e:
-        time.sleep(5)
         msg.send_message(text=esc.escape_shit('Oh and btw, fetching GA data failed and this is why:'), chat_id=os.environ.get('FC_GROUP_CHAT_ID'))
         msg.send_message(text=esc.escape_shit(str(e)), chat_id=os.environ.get('FC_GROUP_CHAT_ID'))
     return esc.escape_shit(text)
