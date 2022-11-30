69118155ef20cf6165dd402e56e3524559b0fefa
ayy-em <mironov-hse@yandex.ru>
2022-11-30 02:33:59 +0100

    #6 I fucked it up, fixed it now

--- utils/ai_response.py
+++ utils/ai_response.py
@@ -3,10 +3,11 @@ import openai
 
 
 def get_ai_response(prompt):
+    prompt = prompt[:50] if len(prompt) > 50 else prompt
     openai.api_key = os.getenv('OPEN_AI_API_KEY')
     response = openai.Completion.create(
         model="text-curie-001",
-        prompt="Hello",
+        prompt=prompt,
         temperature=0,
         max_tokens=50
     )
